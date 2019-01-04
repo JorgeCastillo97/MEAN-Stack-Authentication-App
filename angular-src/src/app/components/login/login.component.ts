b668394b034bb45bb599ac463f438dcac0bce2b3
Jorge Castillo <jorcasjim29@gmail.com>
2019-01-04 00:45:06 -0600

    :sparkles: Login/Logout component working correctly.

--- angular-src/src/app/components/login/login.component.ts
+++ angular-src/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username: String;
+  password: String;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private flashMessagesService: FlashMessagesService
+  ) { }
 
   ngOnInit() {
+  }
+
+  onLoginSubmit() {
+    const user = {
+      username: this.username,
+      password: this.password
+    }
+
+    this.authService.authenticateUser(user).subscribe(data => {
+      if(data.success) {
+        this.authService.storeUserData(data.token, data.user);
+        this.flashMessagesService.show('You are now logged in!' , {cssClass: 'alert-success', timeout: 3000});
+        this.router.navigate(['dashboard']);
+      } else {
+        this.flashMessagesService.show(data.message , {cssClass: 'alert-danger', timeout: 3000});
+        // 'Redirects' to the same page.
+        this.router.navigate(['login']);
+      }
+    });
   }
 
 }
