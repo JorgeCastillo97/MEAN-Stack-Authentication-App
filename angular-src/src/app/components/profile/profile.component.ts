b2c27ae11d093073f5751ae37a47a7232300a29e
Jorge Castillo <jorcasjim29@gmail.com>
2019-01-04 20:09:07 -0600

    :construction: Dashboard and Profile components functionality.

--- angular-src/src/app/components/profile/profile.component.ts
+++ angular-src/src/app/components/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  _id: String
+  name: String;
+  username: String;
+  email: String;
+  password: String;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.authService.getProfile().subscribe(profile => {
+      //alert("id: " + profile._id);
+      this._id = profile._id;
+      //alert("name:" + profile.name);
+      this.name = profile.name;
+      //alert("username:" + profile.username);
+      this.username = profile.username;
+      //alert("email:" + profile.email);
+      this.email = profile.email;
+      //alert("password:" + profile.password);
+      this.password = profile.password;
+    }, err => {
+      console.log(err);
+      return false;
+    });
   }
 
 }
